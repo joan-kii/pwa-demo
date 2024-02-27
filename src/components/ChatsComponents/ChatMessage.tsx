cf78389b35ce5af3e78b57871e1993a47f7cecda
joan-kii <joan.sb@hotmail.com>
2024-02-27 08:35:01 +0100

    solve pdf bug

--- src/components/ChatsComponents/ChatMessage.tsx
+++ src/components/ChatsComponents/ChatMessage.tsx
@@ -12,7 +12,7 @@ function ChatMessage({ message }: { message: Message }) {
       className={`my-2 flex items-end text-gray-700 ${message.author === activeUser.user.name ? 'justify-end' : ''}`}
     >
       <div className={`w-4/5 sm:w-2/3 flex ${message.author === activeUser.user.name ? 'justify-end' : 'justify-start'}`}>
-        <div className={`p-2 flex w-max items-end text-sm sm:text-base ${message.author === activeUser.user.name ? 'bg-green-200' : 'bg-white'} rounded-md`}>
+        <div className={`p-2 flex items-end text-sm sm:text-base ${message.author === activeUser.user.name ? 'bg-green-200' : 'bg-white'} rounded-md`}>
           <TextParsed text={message.text} />
           <p className="ml-2 text-xs text-gray-400">{message.hour}</p>
         </div>
